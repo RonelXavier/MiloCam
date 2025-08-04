3494236a4de62f325a0727ab09a4c80d86d396b5
Ron Xavier <Ronelxavi@gmail.com>
2025-08-04 09:46:29 +0530

    better documentation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# import libraries
 import os
 from transformers import DetrImageProcessor, DetrForObjectDetection
 import torch
@@ -8,12 +9,15 @@ import cv2 as cv
 import discord
 from dotenv import load_dotenv
 
+# initialize keys for discord bot
 load_dotenv()
 token = os.getenv('DiscToken')
 processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
 model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
 
+
 async def compvision():
+    # start up cv
     cap = cv.VideoCapture(0)
     channel_id = int(os.getenv('channelid'))
     channel = client.get_channel(channel_id)
@@ -28,20 +32,18 @@ async def compvision():
             print("can't get the frame. exiting...")
             break
         img = frame
-        # you can specify the revision tag if you don't want the timm dependency
-        
+        # loops through frames, feeding each frame into the processor
         inputs = processor(images=img, return_tensors="pt") 
         outputs = model(**inputs)
 
-        # convert outputs (bounding boxes and class logits) to COCO API
-        # let's only keep detections with score > 0.9
-
-        height, width = img.shape[:2] 
+        # img is a numpy array (shape: (height, width, channels)) so doing this returns a tuple of height and width that can be wrapped to a 2D tensor.
+        height, width = img.shape[:2]
         target_sizes = torch.tensor([[height, width]])
-        #img is a numpy array (shape: (height, width, channels)) so doing this returns a tuple of height and width that can be wrapped to a 2D tensor.
 
+        # convert outputs (bounding boxes and class logits) to COCO API
+        #  only keep detections with score > 0.9
         results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
-
+        # detection code form model example on huggingface
         for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
             box = [round(i, 2) for i in box.tolist()]
             print(
